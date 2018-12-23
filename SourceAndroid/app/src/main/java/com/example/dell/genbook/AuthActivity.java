64db7c219fe6faf7b9352e1fc6ed83f709f46e56
HuyTran1996 <tqhuy1996.developer@gmail.com>
2018-12-23 13:17:56 +0700

    add base source blockchain

--- SourceAndroid/app/src/main/java/com/example/dell/genbook/AuthActivity.java
+++ SourceAndroid/app/src/main/java/com/example/dell/genbook/AuthActivity.java
@@ -2,8 +2,11 @@ package com.example.dell.genbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class AuthActivity extends AppCompatActivity {
+
+    Button btnImportWallet , btnGoGenbook;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
